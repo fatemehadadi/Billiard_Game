82bda1e0a1f6767b1d68104138941f2721ddbb0c
fateme <vagol77@yahoo.com>
2018-07-12 13:46:01 +0000

    Upload New File

---
+++ headers/color.h
@@ -0,0 +1,12 @@
+//
+// Created by pure on 7/12/18.
+//
+
+#ifndef BILL_COLOR_H
+#define BILL_COLOR_H
+class Colour{
+public:
+    Colour(int red, int green, int blue);
+    int r,b,g;
+};
+#endif //BILL_COLOR_H
