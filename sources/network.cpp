bb8dbc5b5bae38752cb9ace9f8701ce0cf011b77
fateme <vagol77@yahoo.com>
2018-07-23 17:56:48 +0000

    Upload New File

---
+++ sources/network.cpp
@@ -0,0 +1,74 @@
+//
+// Created by pure on 7/22/18.
+//
+
+#include "../headers/network.h"
+#include <SFML/Network.hpp>
+
+Network::Network(Resource *resource) {
+    this->resource = resource;
+    this->is_server = true;
+    this->ip = "0.0.0.0";
+
+    this->listener = new sf::TcpListener;
+
+    listener->listen(12345);
+}
+
+Network::Network(Resource *resource, std::string ip) {
+    this->resource = resource;
+    this->is_server = false;
+    this->ip = ip;
+}
+
+void Network::listen() {
+    if (this->is_server) {
+        sf::SocketSelector selector;
+        selector.add(*this->listener);
+        if (selector.wait(sf::seconds(5)))
+        {
+                if (selector.isReady(*this->listener))
+                {
+                    this->socket = new sf::TcpSocket;
+                    this->listener->accept(*this->socket);
+                }
+        }
+
+    }
+}
+
+void Network::connect(Game *game) {
+    if (not this->is_server){
+        this->socket = new sf::TcpSocket;
+        sf::Socket::Status status =this->socket->connect(this->ip, 12345);
+        if (status != sf::Socket::Done)
+        {
+            // error...
+            game->status="No player is available";
+        }
+    }
+}
+
+void Network::send() {
+    std::string message = this->resource->get();
+    char* c_message = new char[(int)message.size() + 1];
+
+    for (int i = 0; i < message.size(); i++)
+        c_message[i] = message[i];
+
+    this->socket->send(c_message, (int)message.size());
+
+    delete c_message;
+}
+
+void Network::receive() {
+    char data[100];
+    size_t received;
+    this->socket->receive(data, 100, received);
+
+    if (received != 0) {
+        std::string message = std::string(data);
+
+        this->resource->set(message);
+    }
+}
