6a7ece6b8da24486ef95bbe751e2632476607055
fateme <vagol77@yahoo.com>
2018-07-23 17:52:24 +0000

    Replace game.h

--- headers/game.h
+++ headers/game.h
@@ -1,13 +1,11 @@
 //
-// Created by pure on 7/12/18.
+// Created by pure on 7/22/18.
 //
 
-#ifndef BILL_GAME_H
-#define BILL_GAME_H
-
+#ifndef SNOOKER_GAME_H
+#define SNOOKER_GAME_H
 #include "ball.h"
 #include "player.h"
-#include "color.h"
 #include <iostream>
 using namespace std;
 
@@ -15,10 +13,15 @@ class Game{
 public:
     Game();
     Player *me,*rival;
-    Ball *b;
-    bool is_your_turn();
-
+    Ball **ball;
+    string status, list;
+    int selectedV=0;
+    bool is_your_turn=true, choose_color, choose_dir, choose_side, choose_speed, is_started, is_chosen, is_listening;
+    bool bulls_stoped();
+    int turn;
+    bool is_new(string);
+    void move();
 
 };
 
-#endif //BILL_GAME_H
+#endif //SNOOKER_GAME_H
