9016fa35f837be6ea842219e7192d328233a3e9b
fateme <vagol77@yahoo.com>
2018-07-23 17:51:49 +0000

    Replace ball.h

--- headers/ball.h
+++ headers/ball.h
@@ -1,9 +1,9 @@
 //
-// Created by pure on 7/12/18.
+// Created by pure on 7/22/18.
 //
 #include <iostream>
-#ifndef BILL_BALL_H
-#define BILL_BALL_H
+#ifndef SNOOKER_BALL_H
+#define SNOOKER_BALL_H
 using namespace std;
 
 class Ball{
@@ -11,9 +11,12 @@ public:
     Ball(string color,float score,float pos_x,float pos_y);
     string color;
     float score;
+    float speed;
+    float fa;
     float p[2];
     float v[2];
     float a[2];
-    bool is_stoped=true;
+    bool is_stoped();
 };
-#endif //BILL_BALL_H
+
+#endif //SNOOKER_BALL_H
