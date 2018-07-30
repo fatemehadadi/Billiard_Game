cf091becc1cf103cf0cb045dd1b2433df5d4de41
fateme <vagol77@yahoo.com>
2018-07-30 04:39:23 +0000

    fix bugs

--- sources/resource.cpp
+++ sources/resource.cpp
@@ -4,6 +4,8 @@
 
 #include "../headers/resource.h"
 
+
+//over write operators for Balls objects in packet
 sf::Packet& operator <<(sf::Packet& packet, Ball *&ball)
 {
     return packet << ball->color<<ball->score<<ball->p[0]<<ball->p[1]<<ball->v[0]<<ball->v[1]<<ball->a[0]<<ball->a[1];
@@ -18,7 +20,7 @@ Resource::Resource(Game *game) {
     this->game = game;
 }
 
-void Resource::set(sf::Packet &pack) { //for network.set
+void Resource::set(sf::Packet &pack) { //for network.set //is client
     /*std::stringstream inp(str);
 
     float p_position, ball_x, ball_y;
@@ -31,6 +33,16 @@ void Resource::set(sf::Packet &pack) { //for network.set
     pack>>str;
     for(int i=0;i<22;i++){
         pack>>game->ball[i];
+    }
+    int temp;
+    pack>>temp;
+    if(temp!=game->turn){
+        game->motion="Your Turn!\nSelect the speed:";
+        game->choose_color=false;
+        game->choose_dir=false;
+        game->choose_side=false;
+        game->choose_speed=false;
+        game->turn=temp;
     }
 }
 sf::Packet Resource::get() {  //for network.send
@@ -47,6 +59,6 @@ sf::Packet Resource::get() {  //for network.send
     for(int i=0;i<22;i++){
         pack<<game->ball[i];
     }
-    pack>>game->spin;
+    pack<<-1*game->turn;
     return pack;
 }
